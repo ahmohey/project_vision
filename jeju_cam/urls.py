7984955bc8f743818f23304f98008acac442b25c
ahmohey <kyh44004@gmail.com>
2023-07-03 16:10:26 +0900

    url add

---
+++ jeju_cam/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from jeju_cam.views import UserView
+app_name = "jeju_cam"
+
+urlpatterns = [
+    path("user/", UserView.as_view(), name="user_list_create"),
+    path("user/<int:pk>/", UserView.as_view(), name="user_detail_update_delete"),
+]
